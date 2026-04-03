2e2fd2761f4dbac4d0cde4e71bfdfc6a78a285e6
machado.py <vitorandroidcwb@gmail.com>
2026-04-03 03:26:55 -0300

    commit note

--- FuelProject/settings.py
+++ FuelProject/settings.py
@@ -137,6 +137,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import redirect, render
 from fuelrequests.models import Fuelrequests
 from fuelrequests.forms import FuelReqForms
+from .models import Usuario
 
 
 # Create your views here.
 def usuarios(request):
         #solicitacoes = Fuelrequests.objects.all() ordenacao somente para teste
         solicitacoes = Fuelrequests.objects.all().order_by('-data_solicitacao')[:9]
-        return render(request, 'usuarios/index.html', {'solicitacoes': solicitacoes})
+        card_usuarios = Usuario.objects.all()
+
+        context = {
+                'solicitacoes': solicitacoes,
+                'card_usuarios' : card_usuarios,
+                'usuario_logado': request.user
+        }
+
+        return render(request, 'usuarios/index.html', context)
 
 
 
 def userlogin(request):
         return render(request, 'usuarios/userlogin.html')
+
+
+
+
